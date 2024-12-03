a7554cb19e6c414a5d1e55592fb15c9f851de41c
williamcioffi <william.cioffi@gmail.com>
2024-12-03 18:07:18 -0500

    col labels working file

---
+++ gonio_col_labels.r
@@ -0,0 +1,25 @@
+goniolabels <- c(
+  "1", "cmd",
+  "2", "fav_ind",
+  "3", "year",
+  "4", "month",
+  "5", "day",
+  "6", "hour",
+  "7", "minute"
+  "8", "second",
+  "9", "hexid",
+  "10", "freq_hz",
+  "11", "validity_8b",
+  "12", "bear_inst_deg",
+  "13", "bear_avg_deg",
+  "14", "level_inst_db",
+  "15", "level_avg_db",
+  "16", "longitude_x60kdeg",
+  "17", "latitude_x60kdeg",
+  "18", "gonio_dir_notimplemented",
+  "19", "msg_len",
+  "20", "msg"
+)
+
+
++ plus mystery col
